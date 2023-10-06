357dccf234e0c84eec1da6a8b3cd7f21e3f527f4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-05 21:35:57 -0300

    Criando controller

---
+++ src/main/java/com/github/brunopioltini/ecommerce/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.github.brunopioltini.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	 @GetMapping("/ping")
+	    public String ping() {
+	        return "pong";
+	    }
+}
